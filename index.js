77c5f8c752a69d4b1d3106e5e7b73c1955c99d4a
Gauravkumar10094559 <karthikkumar10094559@gmail.com>
2017-10-02 12:23:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express'); // common js modules (node has support for common js modules only)
+// import express from 'express'; // es2015 modules (for front end)
+
+const app=express();// app object represents underlying running express server
+
+app.get('/',(req,res)=> {	// handle incoming http request
+	res.send({hi:'there'});
+});
+
+const PORT= process.env.PORT;
+
+app.listen(PORT || 5000 ,() => {
+	console.log('server has started');
+});	// telling node to listen on this port
