b094dcaada9fdc2aefbb86b2e662dd0cb083f2f6
Gauravkumar10094559 <karthikkumar10094559@gmail.com>
2017-10-04 15:04:20 +0530

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -4,12 +4,14 @@ const keys = require("../config/keys");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 
-passport.serializeUser((user,done) => {	//to convert id in to cookie 
-	done(null, user.id);	//we use user.id because a user may or may not have googleID (it can have fb id) but it will have our mlab assigned id. and u can access directly using .id
+passport.serializeUser((user, done) => {
+	//to convert id in to cookie
+	done(null, user.id); //we use user.id because a user may or may not have googleID (it can have fb id) but it will have our mlab assigned id. and u can access directly using .id
 });
 
-passport.deserializeUser((id, done) => {// to use cookie to get back the id
-	User.findById(id)	//find which id user belongs to
+passport.deserializeUser((id, done) => {
+	// to use cookie to get back the id
+	User.findById(id) //find which id user belongs to
 		.then(user => {
 			done(null, user);
 		});
@@ -20,7 +22,12 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
+			proxy:true
 			callbackURL: "/auth/google/callback"
+			//heroku uses proxy to identify the server assigned to use
+			//thats where the https is changed to http
+			// two sol-1.change relative path to absolute(https://flow-df.herorku.com/auth/google)
+			//2.add tell google to let it happen(add another google strategy)
 		},
 		(accessToken, refreshToken, profile, done) => {
 			// console.log("access token", accessToken); ///proof we are allowed to use user info//when user is sent back to the server and for data comes back of user using code
@@ -34,7 +41,7 @@ passport.use(
 				//promise
 				if (existingUser) {
 					// console.log('already exists');
-					done(null, existingUser);//carry on with authentication now we are done with users.null is for no errors
+					done(null, existingUser); //carry on with authentication now we are done with users.null is for no errors
 				} else {
 					new User({
 						googleId: profile.id
